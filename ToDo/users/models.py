925b46f58e32fcf91bce1dbbd2fc9f11f0dbf22c
RomanPM85 <mamchiy@yandex.ru>
2022-09-25 01:00:17 +0300

    create User model

--- ToDo/users/models.py
+++ ToDo/users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import ugettext_lazy as _
 # Create your models here.
+
+
+class User(AbstractUser):
+    user_name = models.CharField(max_length=64)
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    email = models.EmailField(_('email address'), unique=True)
+
